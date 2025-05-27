822d4d35e3afb36be30a6e2a226f25f30b9e251b
VVS Basanth Pedapati <vvs.pedapati@rediffmail.com>
2025-05-27 22:36:04 +0530

    linkFix1

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
             </svg>
           </a>
           <a
-            href="https://linkedin.com/in/basanth-pedapati"
+            href="https://www.linkedin.com/in/basanth-pedapati"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="LinkedIn"
